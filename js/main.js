3e2d8733c850c2042c1a3bddab7ef5e0fb4bc9b1
Adrian O'Grady <elpollouk@gmail.com>
2016-08-16 20:14:59 +0100

    Updated to use ChickenVis

--- js/main.js
+++ js/main.js
@@ -1,35 +1,20 @@
 (function () {
 "use strict";
 
+var UpdateLoop = Chicken.fetch("ChickenVis.UpdateLoop");
+var Loader = Chicken.fetch("ChickenVis.Loader");
+
+var updater = new UpdateLoop(onFrame);
+var loader = new Loader();
+
 var drawCtx;
-var sprites = {
-    entity: "assets/entity-facing.png"
-};
-
-function loadAssets(assets, callback) {
-    var total = 0;
-    var loaded = 0;
-    for (var k in assets) {
-        if (typeof assets[k] == "string") {
-            total++;
-            var id = k;
-            var img = new Image();
-            img.onload = function () {
-                loaded++;
-                assets[id] = function (x, y, w, h) {
-                    w = w || x;
-                    h = h || y;
-                    drawCtx.drawImage(img, x, y, w, h);
-                };
-
-                if (total === loaded) {
-                        callback();
-                }
-            };
-            img.src = assets[k];
-        }
+var sprites = [
+    {
+        id: "entity",
+        source: "assets/entity-facing.png",
+        type: Loader.TYPE_IMAGE
     }
-}
+];
 
 function rect(x, y, w, h, c) {
     drawCtx.fillStyle = c;
@@ -43,17 +28,47 @@ function circle(x, y, r, c) {
     drawCtx.fill();
 }
 
+function image(id, x, y) {
+    var img = loader.getData(id);
+    drawCtx.drawImage(img, x, y);
+}
+
+var x = 50;
+
+function drawFrame() {
+    drawCtx.clearRect(0, 0, 800, 600);
+    rect(10, 10, 280, 130, "rgb(255, 0, 0)");
+    circle(20, 20, 20, "rgb(0, 255, 0)");
+    image("entity", x++, 50);
+}
+
+
 function init(callback) {
     drawCtx = viewer.getContext("2d");
-    loadAssets(sprites, callback);
+    loader.queue(sprites, function () {
+        if (loader.numReady + loader.failed.length === loader.numTotal) {
+            callback(loader.failed.length === 0);
+        }
+    });
+}
+
+function onFrame() {
+    drawFrame();
 }
 
 window.main = function () {
     console.log("Hello World.");
-    init(function () {
-        rect(10, 10, 280, 130, "rgb(255, 0, 0)");
-        circle(20, 20, 20, "rgb(0, 255, 0)");
-        sprites.entity(50, 50);
+    init(function (ok) {
+        if (ok) {
+            updater.paused = false;
+        }
+        else {
+            console.error("Failed to load assets");
+        }
     });
+}
+
+window.updateToggle = function () {
+    updater.paused = !updater.paused;
 }
 })();
