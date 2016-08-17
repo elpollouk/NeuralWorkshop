b8ed258b89c1881e4649623edd623d83433c7bba
Adrian O'Grady <elpollouk@gmail.com>
2016-08-17 11:20:22 +0100

    Chickenised main.js

--- js/main.js
+++ js/main.js
@@ -1,59 +1,57 @@
-(function () {
-"use strict";
+Chicken.inject(["ChickenVis.UpdateLoop", "ChickenVis.Loader", "ChickenVis.Draw"],
+function (UpdateLoop, Loader, Draw) {
+    "use strict";
 
-var UpdateLoop = Chicken.fetch("ChickenVis.UpdateLoop");
-var Loader = Chicken.fetch("ChickenVis.Loader");
-var Draw = Chicken.fetch("ChickenVis.Draw");
+    var updater = new UpdateLoop(onFrame);
+    var loader = new Loader();
+    var draw;
 
-var updater = new UpdateLoop(onFrame);
-var loader = new Loader();
-var draw;
+    var sprites = [
+        {
+            id: "entity",
+            source: "assets/entity-facing.png",
+            type: Loader.TYPE_IMAGE
+        }
+    ];
 
-var sprites = [
-    {
-        id: "entity",
-        source: "assets/entity-facing.png",
-        type: Loader.TYPE_IMAGE
+    function init(callback) {
+        draw = new Draw(viewer, 800, 600);
+        loader.queue(sprites, function () {
+            if (loader.numReady + loader.failed.length === loader.numTotal) {
+                callback(loader.failed.length === 0);
+            }
+        });
     }
-];
 
-function init(callback) {
-    draw = new Draw(viewer, 800, 600);
-    loader.queue(sprites, function () {
-        if (loader.numReady + loader.failed.length === loader.numTotal) {
-            callback(loader.failed.length === 0);
-        }
-    });
-}
+    function onFrame(dt) {
+        drawFrame();
+    }
 
-function onFrame(dt) {
-    drawFrame();
-}
+    var x = 50;
 
-var x = 50;
+    function drawFrame() {
+        draw.clear();
+        draw.rect(10, 10, 280, 130, "rgb(255, 0, 0)");
+        draw.circle(20, 20, 20, "rgb(0, 255, 0)");
+        draw.image(loader.getData("entity"), x++, 50);
 
-function drawFrame() {
-    draw.clear();
-    draw.rect(10, 10, 280, 130, "rgb(255, 0, 0)");
-    draw.circle(20, 20, 20, "rgb(0, 255, 0)");
-    draw.image(loader.getData("entity"), x++, 50);
+        draw.text(`FPS = ${Math.floor(updater.fps)}`, 0, 0);
+    }
 
-    draw.text(`FPS = ${Math.floor(updater.fps)}`, 0, 0);
-}
+    window.onload = function () {
+        console.log("Hello World.");
+        init(function (ok) {
+            if (ok) {
+                updater.paused = false;
+            }
+            else {
+                console.error("Failed to load assets");
+            }
+        });
+    }
 
-window.main = function () {
-    console.log("Hello World.");
-    init(function (ok) {
-        if (ok) {
-            updater.paused = false;
-        }
-        else {
-            console.error("Failed to load assets");
-        }
-    });
-}
+    window.updateToggle = function () {
+        updater.paused = !updater.paused;
+    }
 
-window.updateToggle = function () {
-    updater.paused = !updater.paused;
-}
-})();
+});
